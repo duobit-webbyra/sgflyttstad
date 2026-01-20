63ed9940c80c564a6403e67b3d5f4761e51ab087
Marwin Hormiz <marwinhormiz@gmail.com>
2026-01-20 19:37:15 +0100

    Hårdkodar sitekey

--- src/app/components/Turnstile/index.tsx
+++ src/app/components/Turnstile/index.tsx
@@ -5,7 +5,7 @@ import Turnstile, { useTurnstile } from "react-turnstile";
 export default function TurnstileWidget() {
   return (
     <Turnstile 
-      sitekey={process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY}
+      sitekey={'0x4AAAAAAA5FyHhcY8lrYZcG'}
       className="cf-turnstile"
     />
   );
