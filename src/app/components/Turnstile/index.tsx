8eddf39808157e06fd05b07cd487621ddf254201
Marwin Hormiz <56167333+marwinhormiz@users.noreply.github.com>
2026-01-20 18:20:27 +0000

    Lägger till cf-turnstile i className

--- src/app/components/Turnstile/index.tsx
+++ src/app/components/Turnstile/index.tsx
@@ -5,7 +5,8 @@ import Turnstile, { useTurnstile } from "react-turnstile";
 export default function TurnstileWidget() {
   return (
     <Turnstile 
-      sitekey={process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY} 
+      sitekey={process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY}
+      className="cf-turnstile"
     />
   );
 }
